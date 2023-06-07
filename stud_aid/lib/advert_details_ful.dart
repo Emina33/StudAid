aac1c165529a9a269737ffe8a7882df1528d488d
Emina33 <emi.handzic@gmail.com>
2023-06-07 19:35:05 +0200

    chat+payment

--- stud_aid/lib/advert_details_ful.dart
+++ stud_aid/lib/advert_details_ful.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:stud_aid/PaypalPayment.dart';
 import 'package:stud_aid/home_page.dart';
 import 'package:stud_aid/models/advert.dart';
 import 'package:stud_aid/models/reservation.dart';
@@ -8,7 +9,7 @@ import 'package:stud_aid/payment_page.dart';
 import 'package:stud_aid/providers/advert_provider.dart';
 import 'package:stud_aid/providers/reservation_provider.dart';
 import 'package:stud_aid/review_page.dart';
-
+import 'package:flutter_paypal/flutter_paypal.dart';
 import 'advertDetails.dart';
 import 'components/bottom_bar.dart';
 import 'components/top_bar.dart';
@@ -100,7 +101,66 @@ class _AdvertDetailsFulState extends State<AdvertDetailsFul> {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) => const PaymentPage()),
+                            builder: (context) => UsePaypal(
+                                sandboxMode: true,
+                                clientId:
+                                    "AW1TdvpSGbIM5iP4HJNI5TyTmwpY9Gv9dYw8_8yW5lYIbCqf326vrkrp0ce9TAqjEGMHiV3OqJM_aRT0",
+                                secretKey:
+                                    "EHHtTDjnmTZATYBPiGzZC_AZUfMpMAzj2VZUeqlFUrRJA_C0pQNCxDccB5qoRQSEdcOnnKQhycuOWdP9",
+                                returnURL: "https://samplesite.com/return",
+                                cancelURL: "https://samplesite.com/cancel",
+                                transactions: const [
+                                  {
+                                    "amount": {
+                                      "total": '1.00',
+                                      "currency": "USD",
+                                      "details": {
+                                        "subtotal": '1.00',
+                                        "shipping": '0',
+                                        "shipping_discount": 0
+                                      }
+                                    },
+                                    "description":
+                                        "The payment transaction description.",
+                                    // "payment_options": {
+                                    //   "allowed_payment_method":
+                                    //       "INSTANT_FUNDING_SOURCE"
+                                    // },
+                                    "item_list": {
+                                      "items": [
+                                        {
+                                          "name": "A demo product",
+                                          "quantity": 1,
+                                          "price": '1.00',
+                                          "currency": "USD"
+                                        }
+                                      ],
+
+                                      // shipping address is not required though
+                                      "shipping_address": {
+                                        "recipient_name": "Jane Foster",
+                                        "line1": "Travis County",
+                                        "line2": "",
+                                        "city": "Austin",
+                                        "country_code": "US",
+                                        "postal_code": "73301",
+                                        "phone": "+00000000",
+                                        "state": "Texas"
+                                      },
+                                    }
+                                  }
+                                ],
+                                note:
+                                    "Contact us for any questions on your order.",
+                                onSuccess: (Map params) async {
+                                  print("onSuccess: $params");
+                                },
+                                onError: (error) {
+                                  print("onError: $error");
+                                },
+                                onCancel: (params) {
+                                  print('cancelled: $params');
+                                })),
                       );
                     },
                     style: ElevatedButton.styleFrom(
